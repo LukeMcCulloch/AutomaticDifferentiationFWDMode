57aab7893613b3a8bf593253d2de636643f791e3
Luke McCulloch <tlukemcculloch@gmail.com>
2020-01-26 16:15:14 -0700

    basic data

--- src/AutomaticDifferentiation.cpp
+++ src/AutomaticDifferentiation.cpp
@@ -10,10 +10,17 @@
 typedef float Number;
 
 using Eigen::Dynamic;
-//using Eigen::MatrixXd; // equivalent to:  typedef Matrix<double, Dynamic, Dynamic> MatrixXd;
-//using Eigen::VectorXd;
-//typedef Eigen::Matrix<Number, Dynamic, 1> Vector;
 
+
+//https://eigen.tuxfamily.org/dox/TopicFunctionTakingEigenTypes.html
+//By letting your function take templated parameters of these base types, 
+//you can let them play nicely with Eigen's expression templates.
+template <typename Derived>
+void print_size(const Eigen::EigenBase<Derived>& b)
+{
+  std::cout << "size (rows, cols): " << b.size() << " (" << b.rows()
+            << ", " << b.cols() << ")" << std::endl;
+}
 
 class AD {
 
@@ -22,6 +29,18 @@ class AD {
    Number value;
    Eigen::Matrix<Number, Dynamic, 1> grad;
    Eigen::Matrix<Number, Dynamic, Dynamic> hess;
+
+
+   Eigen::Matrix<int, 2, 1> shape;
+
+   int space_dim;
+
+   // location in the gradient space - i.e. where this variable "lives"
+   // in relation to the otheres in a design space.
+   int index;
+
+   // AD variables can have a name
+   std::string name;
 
 };
 
